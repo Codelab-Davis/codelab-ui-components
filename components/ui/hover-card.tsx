340f7de530fc82f25060ad72b825f6be97fdb270
Eggbask <ceuzjonathan@gmail.com>
2025-02-22 14:21:24 -0800

    Begin documentation

--- components/ui/hover-card.tsx
+++ components/ui/hover-card.tsx
@@ -2,9 +2,8 @@
 
 import * as React from "react";
 import * as HoverCardPrimitive from "@radix-ui/react-hover-card";
-
 import { cn } from "@/lib/utils";
-
+import { cva, type VariantProps } from "class-variance-authority";
 import { CalendarDays } from "lucide-react";
 
 const HoverCard = HoverCardPrimitive.Root;
@@ -30,9 +29,28 @@ interface HoverCardContentProps
     date?: string;
 }
 
+const HoverCardContentVarients = cva(
+    "z-96 rounded-lg border pt-4 pb-4 pr-7 pl-7 mt-2.5 flex gap-6 text-popover-foreground shadow-md outline-none data-[state=open]:animate-in data-[state=closed]:animate-out data-[state=closed]:fade-out-0 data-[state=open]:fade-in-0 data-[state=closed]:zoom-out-95 data-[state=open]:zoom-in-95 data-[side=bottom]:slide-in-from-top-2 data-[side=left]:slide-in-from-right-2 data-[side=right]:slide-in-from-left-2 data-[side=top]:slide-in-from-bottom-2",
+    {
+        variants: {
+            variant: {
+                default: "bg-popover border-[#E4E4E7]"
+            },
+            size: {
+                default: "w-[300.58px] h-34"
+            }
+        },
+
+        defaultVariants: {
+            variant: "default",
+            size: "default"
+        }
+    }
+)
+
 const HoverCardContent = React.forwardRef<
     React.ComponentRef<typeof HoverCardPrimitive.Content>,
-    HoverCardContentProps
+    HoverCardContentProps & VariantProps<typeof HoverCardContentVarients>
 >(
     (
         {
@@ -42,6 +60,8 @@ const HoverCardContent = React.forwardRef<
             title,
             description,
             date,
+            variant,
+            size,
             children,
             ...props
         },
@@ -52,24 +72,17 @@ const HoverCardContent = React.forwardRef<
             align={align}
             sideOffset={sideOffset}
             className={cn(
-                "z-96 w-[300.58px] h-34 rounded-lg border border-[#E4E4E7] bg-popover pt-4 pb-4 pr-7 pl-7 mt-2.5 flex gap-6 text-popover-foreground shadow-md outline-none data-[state=open]:animate-in data-[state=closed]:animate-out data-[state=closed]:fade-out-0 data-[state=open]:fade-in-0 data-[state=closed]:zoom-out-95 data-[state=open]:zoom-in-95 data-[side=bottom]:slide-in-from-top-2 data-[side=left]:slide-in-from-right-2 data-[side=right]:slide-in-from-left-2 data-[side=top]:slide-in-from-bottom-2",
+                HoverCardContentVarients({variant, size}),
                 className
             )}
             {...props}
         >
             <div className="bg-black w-[27px] h-[27px] rounded-full py-[8.7px] px-[8.7px] relative">
                 <div className="absolute bottom-0.5">
-                    <span
-                        style={{
-                            color: "#F26F71",
-                            fontWeight: "bold",
-                        }}
-                    >
+                    <span className="text-[#F26F71] font-bold">
                         .
                     </span>
-                    <span
-                        style={{ color: "#FFF", fontWeight: "bold" }}
-                    >
+                    <span className="text-[#FFF] font-bold">
                         /
                     </span>
                 </div>
@@ -77,7 +90,7 @@ const HoverCardContent = React.forwardRef<
             <div className="flex flex-col gap-6">
                 <div className="flex flex-col gap-2">
                     {title && (
-                        <p className="font-bold text-xs/4.5">
+                        <p className="text-xs/4.5 font-bold">
                             {title}
                         </p>
                     )}
@@ -106,4 +119,4 @@ const HoverCardContent = React.forwardRef<
 );
 HoverCardContent.displayName = HoverCardPrimitive.Content.displayName;
 
-export { HoverCard, HoverCardTrigger, HoverCardContent, };
+export { HoverCard, HoverCardTrigger, HoverCardContent };
