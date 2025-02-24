bcb33c821ebdbb8b1eb538974b6f542fb55657e6
EugTe <65484340+EugTe@users.noreply.github.com>
2025-02-23 22:02:06 -0800

    Added Slider Component and Documentation

--- components/ui/slider.tsx
+++ components/ui/slider.tsx
@@ -1,22 +1,20 @@
-"use client"
+"use client";
 
-import * as React from "react"
-import * as SliderPrimitive from "@radix-ui/react-slider"
-import { cva, type VariantProps } from "class-variance-authority"
-import { cn } from "@/lib/utils"
+import * as React from "react";
+import * as SliderPrimitive from "@radix-ui/react-slider";
+import { cva, type VariantProps } from "class-variance-authority";
+import { cn } from "@/lib/utils";
 
 const sliderVariants = cva("relative flex w-full touch-none select-none items-center", {
   variants: {
-    variant: {
-      default: "[&_.slider-track]:bg-gradient-to-r [&_.slider-track]:from-[#FF9B3E] [&_.slider-track]:to-[#FF343B]",
-    },
     size: {
-      default: "[&_.slider-track]:h-[6px] [&_.slider-thumb]:h-[20.25px] [&_.slider-thumb]:w-[20.25px] [&_.tool-tip]:min-w-[34.5px] [&_.tool-tip]:text-[24px] [&_.tool-tip-bot]:w-4 [&_.tool-tip-bot]:h-4",
+      default:
+        "[&_.slider-track]:h-[6px] [&_.slider-thumb]:h-[20.25px] [&_.slider-thumb]:w-[20.25px] [&_.tool-tip]:min-w-[34.5px] [&_.tool-tip]:text-[24px] [&_.tool-tip-bot]:w-4 [&_.tool-tip-bot]:h-4",
       sm: "[&_.slider-track]:h-[4px] [&_.slider-thumb]:h-[13.5px] [&_.slider-thumb]:w-[13.5px] [&_.tool-tip]:min-w-[23px] [&_.tool-tip]:text-[16px] [&_.tool-tip-bot]:w-2 [&_.tool-tip-bot]:h-2",
-      lg: "[&_.slider-track]:h-[8px] [&_.slider-thumb]:h-[27px] [&_.slider-thumb]:w-[27px]  [&_.tool-tip]:min-w-[46px] [&_.tool-tip]:text-[32px] [&_.tool-tip-bot]:w-8 [&_.tool-tip-bot]:h-8",
+      lg: "[&_.slider-track]:h-[12px] [&_.slider-thumb]:h-[27px] [&_.slider-thumb]:w-[27px]  [&_.tool-tip]:min-w-[46px] [&_.tool-tip]:text-[32px] [&_.tool-tip-bot]:w-8 [&_.tool-tip-bot]:h-8",
     },
     hasLabels: {
-      true: "mx-auto",
+      true: "mx-auto w-[calc(98%)]",
       false: "mx-0 w-full",
     },
     hasTooltip: {
@@ -25,59 +23,69 @@ const sliderVariants = cva("relative flex w-full touch-none select-none items-ce
     },
   },
   defaultVariants: {
-    variant: "default",
     size: "default",
     hasLabels: false,
     hasTooltip: false,
   },
-})
+});
 
 interface SliderProps
   extends React.ComponentPropsWithoutRef<typeof SliderPrimitive.Root>,
     VariantProps<typeof sliderVariants> {
-  showTooltip?: boolean
-  tooltipContent?: (value: number[]) => React.ReactNode
+  showTooltip?: boolean;
+  tooltipContent?: (value: number[]) => React.ReactNode;
 }
 
-const Slider = React.forwardRef<React.ElementRef<typeof SliderPrimitive.Root>, SliderProps>(
+const Slider = React.forwardRef<
+  React.ElementRef<typeof SliderPrimitive.Root>,
+  SliderProps
+>(
   (
-    { className, variant, size, hasLabels, showTooltip, tooltipContent, value, defaultValue = [0], ...props },
+    { className, size, hasLabels, showTooltip, tooltipContent, value, defaultValue = [0], ...props },
     ref
   ) => {
-    const [localValue, setLocalValue] = React.useState(value || defaultValue)
+    const isControlled = value !== undefined;
+    const [internalValue, setInternalValue] = React.useState<number[]>(defaultValue);
+    const sliderValue = isControlled ? value : internalValue;
 
-    React.useEffect(() => {
-      if (value) setLocalValue(value)
-    }, [value])
+    const handleValueChange = React.useCallback(
+      (newValue: number[]) => {
+        if (!isControlled) {
+          setInternalValue(newValue);
+        }
+        props.onValueChange?.(newValue);
+      },
+      [isControlled, props]
+    );
 
     return (
       <div className="relative py-2">
         <SliderPrimitive.Root
+          {...props}
           ref={ref}
-          value={value}
-          defaultValue={defaultValue}
-          onValueChange={(newValue) => {
-            setLocalValue(newValue)
-            props.onValueChange?.(newValue)
-          }}
+          value={sliderValue}
+          onValueChange={handleValueChange}
           className={cn(
-            sliderVariants({ variant, size, hasLabels, hasTooltip: showTooltip }),
+            sliderVariants({ size, hasLabels, hasTooltip: showTooltip }),
+            "mx-auto",
             className
           )}
-          {...props}
+          
         >
           {hasLabels && (
             <>
-              <div className="absolute font-[Poppins] -left-4 text-sm text-[#333333]">
+              <div className="absolute font-[Poppins] text-sm text-[#333333] left-0  transform top-1/2
+                -translate-y-1/2 -translate-x-full -ml-2">
                 {props.min || 0}
               </div>
-              <div className="absolute font-[Poppins] -right-7 text-sm text-[#333333]">
+              <div className="absolute font-[Poppins] text-sm text-[#333333] right-0 transform top-1/2
+                -translate-y-1/2  translate-x-full ml-2">
                 {props.max || 100}
               </div>
             </>
           )}
 
-          <SliderPrimitive.Track className="slider-track relative w-full grow overflow-hidden rounded-full">
+          <SliderPrimitive.Track className="slider-track relative w-full min-w-[300px] grow overflow-hidden rounded-full bg-gradient-to-r from-[#FF9B3E] to-[#FF343B] h-[6px] px-2">
             <SliderPrimitive.Range className="absolute h-full bg-transparent" />
           </SliderPrimitive.Track>
 
@@ -86,16 +94,14 @@ const Slider = React.forwardRef<React.ElementRef<typeof SliderPrimitive.Root>, S
             transition-transform focus-visible:outline-none active:scale-110 disabled:pointer-events-none disabled:opacity-50"
           >
             {showTooltip && (
-              <div className="absolute bottom-[calc(160%)] left-[49%] -translate-x-1/2">
+              <div className="absolute bottom-[calc(160%)] left-1/2 -translate-x-1/2">
                 <div className="relative">
-                  {/* Tooltip body with higher z-index */}
                   <div className="relative z-10 tool-tip inline-flex 
                   items-center justify-center rounded-[4px] 
                   bg-gradient-to-r from-[#FF9B3E] to-[#FF343B] 
                   text-white font-[Poppins] font-semibold">
-                    {tooltipContent ? tooltipContent(localValue) : localValue[0]}
+                    {tooltipContent ? tooltipContent(sliderValue) : sliderValue[0]}
                   </div>
-                  {/* Triangle pointer positioned as a sibling with lower z-index */}
                   <div className="absolute -bottom-[3px] left-1/2 -translate-x-1/2 z-0">
                     <div className="tool-tip-bot aspect-1/1 bg-gradient-to-br from-[#FF9B3E] to-[#FF343B] rotate-315 rounded-[2px]" />
                   </div>
@@ -105,9 +111,9 @@ const Slider = React.forwardRef<React.ElementRef<typeof SliderPrimitive.Root>, S
           </SliderPrimitive.Thumb>
         </SliderPrimitive.Root>
       </div>
-    )
+    );
   }
-)
-Slider.displayName = SliderPrimitive.Root.displayName
+);
+Slider.displayName = SliderPrimitive.Root.displayName;
 
-export { Slider, type SliderProps }
+export { Slider, type SliderProps };
