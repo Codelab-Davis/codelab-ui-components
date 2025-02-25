5e5d133b3048987d28a4dbb4f9cadf18fe53595a
munneth <mugill@ucdavis.edu>
2025-02-25 10:09:38 -0800

    added storybook

--- app/page.tsx
+++ app/page.tsx
@@ -1,101 +1,197 @@
-import Image from "next/image";
+"use client";
+/**
+ * @author //Munneth
+ * @detail General Menu Nav bar taken from the ShadCn UI library
+ */
 
-export default function Home() {
-    return (
-        <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-            <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
-                <Image
-                    className="dark:invert"
-                    src="/next.svg"
-                    alt="Next.js logo"
-                    width={180}
-                    height={38}
-                    priority
-                />
-                <ol className="list-inside list-decimal text-sm text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
-                    <li className="mb-2">
-                        Get started by editing{" "}
-                        <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-semibold">
-                            app/page.tsx
-                        </code>
-                        .
-                    </li>
-                    <li>Save and see your changes instantly.</li>
-                </ol>
+//imports
+import * as React from "react";
+import Link from "next/link";
+import { cn } from "@/lib/utils";
+import { Icons } from "@/components/icons";
+import {
+  NavigationMenu,
+  NavigationMenuContent,
+  NavigationMenuItem,
+  NavigationMenuLink,
+  NavigationMenuList,
+  NavigationMenuTrigger,
+  navigationMenuTriggerStyle,
+} from "@/components/ui/navigation-menu";
 
-                <div className="flex gap-4 items-center flex-col sm:flex-row">
-                    <a
-                        className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5"
-                        href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-                        target="_blank"
-                        rel="noopener noreferrer"
-                    >
-                        <Image
-                            className="dark:invert"
-                            src="/vercel.svg"
-                            alt="Vercel logomark"
-                            width={20}
-                            height={20}
-                        />
-                        Deploy now
-                    </a>
-                    <a
-                        className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:min-w-44"
-                        href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-                        target="_blank"
-                        rel="noopener noreferrer"
-                    >
-                        Read our docs
-                    </a>
-                </div>
-            </main>
-            <footer className="row-start-3 flex gap-6 flex-wrap items-center justify-center">
-                <a
-                    className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-                    href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-                    target="_blank"
-                    rel="noopener noreferrer"
+//necesaary components
+const drop1: { title: string; href: string; description: string }[] = [
+  {
+    title: "Content 1",
+    href: "/docs/primitives/alert-dialog",
+    description: "This is a short description of the content.",
+  },
+  {
+    title: "Content 2",
+    href: "/docs/primitives/alert-dialog",
+    description: "This is a short description of the content.",
+  },
+  {
+    title: "Content 3",
+    href: "/docs/primitives/alert-dialog",
+    description: "This is a short description of the content.",
+  },
+  {
+    title: "Content 4",
+    href: "/docs/primitives/alert-dialog",
+    description: "This is a short description of the content.",
+  },
+  {
+    title: "Content 5",
+    href: "/docs/primitives/alert-dialog",
+    description: "This is a short description of the content.",
+  },
+];
+
+const drop2: { title: string; href: string; description: string }[] = [
+  {
+    title: "Content 1",
+    href: "/docs/primitives/alert-dialog",
+    description: "This is a short description of the content.",
+  },
+  {
+    title: "Content 2",
+    href: "/docs/primitives/alert-dialog",
+    description: "This is a short description of the content.",
+  },
+  {
+    title: "Content 3",
+    href: "/docs/primitives/alert-dialog",
+    description: "This is a short description of the content.",
+  },
+  {
+    title: "Content 4",
+    href: "/docs/primitives/alert-dialog",
+    description: "This is a short description of the content.",
+  },
+  {
+    title: "Content 5",
+    href: "/docs/primitives/alert-dialog",
+    description: "This is a short description of the content.",
+  },
+];
+
+const drop3: { title: string; href: string; description: string }[] = [
+  {
+    title: "Content 1",
+    href: "/docs/primitives/alert-dialog",
+    description: "This is a short description of the content.",
+  },
+  {
+    title: "Content 2",
+    href: "/docs/primitives/alert-dialog",
+    description: "This is a short description of the content.",
+  },
+  {
+    title: "Content 3",
+    href: "/docs/primitives/alert-dialog",
+    description: "This is a short description of the content.",
+  },
+  {
+    title: "Content 4",
+    href: "/docs/primitives/alert-dialog",
+    description: "This is a short description of the content.",
+  },
+  {
+    title: "Content 5",
+    href: "/docs/primitives/alert-dialog",
+    description: "This is a short description of the content.",
+  },
+];
+
+//Init menu contents taken from ShadCN site
+function NavigationMenuDemo() {
+  return (
+    <NavigationMenu>
+      <NavigationMenuList>
+        <NavigationMenuItem>
+          <NavigationMenuTrigger>Drop 1</NavigationMenuTrigger>
+          <NavigationMenuContent>
+            <ul className="grid w-[400px] gap-3 p-4 md:w-[500px] md:grid-cols-2 lg:w-[600px] ">
+              {drop1.map((component) => (
+                <ListItem
+                  key={component.title}
+                  title={component.title}
+                  href={component.href}
                 >
-                    <Image
-                        aria-hidden
-                        src="/file.svg"
-                        alt="File icon"
-                        width={16}
-                        height={16}
-                    />
-                    Learn
-                </a>
-                <a
-                    className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-                    href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-                    target="_blank"
-                    rel="noopener noreferrer"
+                  {component.description}
+                </ListItem>
+              ))}
+            </ul>
+          </NavigationMenuContent>
+        </NavigationMenuItem>
+
+        <NavigationMenuItem>
+          <NavigationMenuTrigger>Drop 2</NavigationMenuTrigger>
+          <NavigationMenuContent>
+            <ul className="grid w-[400px] gap-3 p-4 md:w-[500px] md:grid-cols-2 lg:w-[600px] ">
+              {drop2.map((component) => (
+                <ListItem
+                  key={component.title}
+                  title={component.title}
+                  href={component.href}
                 >
-                    <Image
-                        aria-hidden
-                        src="/window.svg"
-                        alt="Window icon"
-                        width={16}
-                        height={16}
-                    />
-                    Examples
-                </a>
-                <a
-                    className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-                    href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-                    target="_blank"
-                    rel="noopener noreferrer"
+                  {component.description}
+                </ListItem>
+              ))}
+            </ul>
+          </NavigationMenuContent>
+        </NavigationMenuItem>
+
+        <NavigationMenuItem>
+          <NavigationMenuTrigger>Drop 3</NavigationMenuTrigger>
+          <NavigationMenuContent>
+            <ul className="grid w-[400px] gap-3 p-4 md:w-[500px] md:grid-cols-2 lg:w-[600px] ">
+              {drop3.map((component) => (
+                <ListItem
+                  key={component.title}
+                  title={component.title}
+                  href={component.href}
                 >
-                    <Image
-                        aria-hidden
-                        src="/globe.svg"
-                        alt="Globe icon"
-                        width={16}
-                        height={16}
-                    />
-                    Go to nextjs.org →
-                </a>
-            </footer>
-        </div>
-    );
+                  {component.description}
+                </ListItem>
+              ))}
+            </ul>
+          </NavigationMenuContent>
+        </NavigationMenuItem>
+      </NavigationMenuList>
+    </NavigationMenu>
+  );
 }
+
+/**!!IMPORTANT!!
+ * when exported with the function call, faced an error of the export not being a react component
+ * was fixed by exporting the function after its call below
+ */
+export default NavigationMenuDemo;
+
+const ListItem = React.forwardRef<
+  React.ElementRef<"a">,
+  React.ComponentPropsWithoutRef<"a">
+>(({ className, title, children, ...props }, ref) => {
+  return (
+    <li>
+      <NavigationMenuLink asChild>
+        <a
+          ref={ref}
+          className={cn(
+            "block select-none space-y-1 rounded-md p-3 leading-none no-underline outline-none transition-colors hover:bg-accent hover:text-accent-foreground focus:bg-accent focus:text-accent-foreground",
+            className,
+          )}
+          {...props}
+        >
+          <div className="text-sm font-medium leading-none">{title}</div>
+          <p className="line-clamp-2 text-sm leading-snug text-muted-foreground">
+            {children}
+          </p>
+        </a>
+      </NavigationMenuLink>
+    </li>
+  );
+});
+ListItem.displayName = "ListItem";
