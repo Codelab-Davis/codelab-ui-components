0114e1122d8c42ec3739f342b399e045d55028e0
Eggbask <ceuzjonathan@gmail.com>
2025-03-05 11:33:59 -0800

    fix(hover-card files): Removed default content in order to make component dynamic. Changed docs accordingly.

--- components/ui/hover-card.tsx
+++ components/ui/hover-card.tsx
@@ -4,7 +4,6 @@ import * as React from "react";
 import * as HoverCardPrimitive from "@radix-ui/react-hover-card";
 import { cn } from "@/lib/utils";
 import { cva, type VariantProps } from "class-variance-authority";
-import { CalendarDays } from "lucide-react";
 
 const HoverCard = HoverCardPrimitive.Root;
 
@@ -14,55 +13,45 @@ const HoverCardTrigger = React.forwardRef<
 >(({ className, ...props }, ref) => (
     <HoverCardPrimitive.Trigger
         ref={ref}
-        className={cn("font-bold underline text-xs/4.5", className)} // Apply bold and underline
+        className={cn("font-bold underline text-xs/4.5", className)}
         {...props}
     />
 ));
 HoverCardTrigger.displayName = HoverCardPrimitive.Trigger.displayName;
 
-interface HoverCardContentProps
-    extends React.ComponentPropsWithoutRef<
-        typeof HoverCardPrimitive.Content
-    > {
-    title?: string;
-    description?: string;
-    date?: string;
-}
-
 const HoverCardContentVarients = cva(
-    "z-96 rounded-lg border pt-4 pb-4 pr-7 pl-7 mt-2.5 flex gap-6 text-popover-foreground shadow-md outline-none data-[state=open]:animate-in data-[state=closed]:animate-out data-[state=closed]:fade-out-0 data-[state=open]:fade-in-0 data-[state=closed]:zoom-out-95 data-[state=open]:zoom-in-95 data-[side=bottom]:slide-in-from-top-2 data-[side=left]:slide-in-from-right-2 data-[side=right]:slide-in-from-left-2 data-[side=top]:slide-in-from-bottom-2",
+    "z-96 rounded-lg border pt-4 pb-4 pr-7 pl-7 mt-2.5 text-popover-foreground shadow-md outline-none data-[state=open]:animate-in data-[state=closed]:animate-out data-[state=closed]:fade-out-0 data-[state=open]:fade-in-0 data-[state=closed]:zoom-out-95 data-[state=open]:zoom-in-95 data-[side=bottom]:slide-in-from-top-2 data-[side=left]:slide-in-from-right-2 data-[side=right]:slide-in-from-left-2 data-[side=top]:slide-in-from-bottom-2",
     {
         variants: {
             variant: {
-                default: "bg-popover border-[#E4E4E7]"
+                default: "bg-popover border-[#E4E4E7]",
             },
             size: {
-                default: "w-[300.58px] h-34"
-            }
+                default: "w-[300.58px] h-34",
+            },
         },
 
         defaultVariants: {
             variant: "default",
-            size: "default"
-        }
+            size: "default",
+        },
     }
-)
+);
 
 const HoverCardContent = React.forwardRef<
     React.ComponentRef<typeof HoverCardPrimitive.Content>,
-    HoverCardContentProps & VariantProps<typeof HoverCardContentVarients>
+    React.ComponentPropsWithoutRef<
+        typeof HoverCardPrimitive.Content
+    > &
+        VariantProps<typeof HoverCardContentVarients>
 >(
     (
         {
             className,
             align = "center",
             sideOffset = 4,
-            title,
-            description,
-            date,
             variant,
             size,
-            children,
             ...props
         },
         ref
@@ -72,48 +61,12 @@ const HoverCardContent = React.forwardRef<
             align={align}
             sideOffset={sideOffset}
             className={cn(
-                HoverCardContentVarients({variant, size}),
+                HoverCardContentVarients({ variant, size }),
                 className
             )}
             {...props}
         >
-            <div className="bg-black w-[30px] h-[27.38px] rounded-full py-[6.83px] px-[8.7px] text-center relative">
-                
-                    <span className="absolute text-[#F26F71] font-black left-2 bottom-0.25">
-                        .
-                    </span>
-                    <span className="absolute text-[#FFF] font-black bottom-1.5 text-xs">
-                        /
-                    </span>
-                
-            </div>
-            <div className="flex flex-col gap-6">
-                <div className="flex flex-col gap-2">
-                    {title && (
-                        <p className="text-xs/4.5 font-bold">
-                            {title}
-                        </p>
-                    )}
-                    {description && (
-                        <p className="text-xs/4.5 font-medium">
-                            {description}
-                        </p>
-                    )}
-                </div>
-                {date && (
-                    <div className="flex gap-2.5">
-                        <CalendarDays
-                            width={15}
-                            height={15}
-                            stroke="#71717A"
-                        ></CalendarDays>
-                        <p className="text-xs/4.5 font-medium text-[#71717A]">
-                            {date}
-                        </p>
-                    </div>
-                )}
-            </div>
-            {children}
+        
         </HoverCardPrimitive.Content>
     )
 );
--- stories/HoverCard/HoverCard.stories.tsx
+++ stories/HoverCard/HoverCard.stories.tsx
@@ -4,6 +4,7 @@ import {
     HoverCardTrigger,
     HoverCardContent,
 } from "@/components/ui/hover-card";
+import { CalendarDays } from "lucide-react";
 
 const meta = {
     title: "UI/HoverCard",
@@ -24,10 +25,41 @@ export const Default: Story = {
                 <HoverCardContent
                     variant="default"
                     size="default"
-                    title="@codelabdavis"
-                    description="Software and Design Agency at UC Davis"
-                    date="Joined December 2021"
-                ></HoverCardContent>
+                >
+                    <div className="flex gap-6">
+                        <div className="bg-black w-[30px] h-[27.38px] rounded-full py-[6.83px] px-[8.7px] text-center relative">
+                            <span className="absolute text-[#F26F71] font-black left-2 bottom-0.25">
+                                .
+                            </span>
+                            <span className="absolute text-[#FFF] font-black bottom-1.5 text-xs">
+                                /
+                            </span>
+                        </div>
+                        <div className="flex flex-col gap-6">
+                            <div className="flex flex-col gap-2">
+                                <p className="text-xs/4.5 font-bold">
+                                    @codelabdavis
+                                </p>
+
+                                <p className="text-xs/4.5 font-medium">
+                                    Software and Design Agency at UC Davis
+                                </p>
+                            </div>
+
+                            <div className="flex gap-2.5">
+                                <CalendarDays
+                                    width={15}
+                                    height={15}
+                                    stroke="#71717A"
+                                ></CalendarDays>
+
+                                <span className="text-xs/4.5 text-[#71717A] font-medium">
+                                    Joined December 2021
+                                </span>
+                            </div>
+                        </div>
+                    </div>
+                </HoverCardContent>
             </>
         ),
     },
