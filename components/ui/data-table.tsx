cfd8cb9fa09b4711f63ffd2196dff504da09d2d8
Sreya Mathew <ssmathew@ucdavis.edu>
2025-03-16 20:45:58 -0700

    docs(data-table): made prop to receive data to be rendered by the DataTable and added DataTable to storybook

--- components/ui/data-table.tsx
+++ components/ui/data-table.tsx
@@ -36,39 +36,6 @@ import {
     TableRow,
 } from "@/components/ui/table";
 
-const data: Payment[] = [
-    {
-        id: "m5gr84i9",
-        amount: 316,
-        status: "success",
-        email: "ken99@example.com",
-    },
-    {
-        id: "3u1reuv4",
-        amount: 242,
-        status: "success",
-        email: "Abe45@example.com",
-    },
-    {
-        id: "derv1ws0",
-        amount: 837,
-        status: "processing",
-        email: "Monserrat44@example.com",
-    },
-    {
-        id: "5kma53ae",
-        amount: 874,
-        status: "success",
-        email: "Silas22@example.com",
-    },
-    {
-        id: "bhqecj4p",
-        amount: 721,
-        status: "failed",
-        email: "carmella@example.com",
-    },
-];
-
 export type Payment = {
     id: string;
     amount: number;
@@ -207,7 +174,11 @@ export const columns: ColumnDef<Payment>[] = [
     },
 ];
 
-export function DataTableDemo() {
+type DataTableProps = {
+    data: Payment[];
+};
+
+export function DataTable({ data } : DataTableProps) {
     const [sorting, setSorting] = React.useState<SortingState>([]);
     const [columnFilters, setColumnFilters] =
         React.useState<ColumnFiltersState>([]);
